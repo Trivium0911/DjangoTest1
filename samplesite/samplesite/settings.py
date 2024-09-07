e9abc043686ce6d5bf614e1aa2a0ea5b8a3f9acd
Trivium0911 <triv_1993@mail.ru>
2024-09-07 13:13:47 +0300

    first commit1

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-od*0mkw(rpme3a#u#&jz%ydw=gh=d068$aaol6_g=)-huyehfy'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
