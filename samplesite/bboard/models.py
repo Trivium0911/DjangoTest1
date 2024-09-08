8852ebdf59c12202e260a62dc0d7e1ca088359fc
Trivium0911 <triv_1993@mail.ru>
2024-09-08 12:33:17 +0300

    add model Bd to models.py in bboard app, make migrations

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Bd(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(default=' ')
+    price = models.FloatField(default=0.0)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+
