b0b93aa976c17578c476f6f6b03941a845f2db1a
Trivium0911 <triv_1993@mail.ru>
2024-09-25 20:07:25 +0300

    change index view with loader

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Bd
 
 
 def index(request):
-    s = 'Объявления \r\n\r\n\r\n'
-    for bb in Bd.objects.order_by('-published'):
-        s += (bb.title + '\r\n' + bb.content + '\r\n' +
-              str(bb.price) + '\r\n\r\n')
-    return HttpResponse(s, content_type='text/plain; charset=utf-8-sig')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bd.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
+
 
