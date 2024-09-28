01c874e0e54a30b398d8abd7c5f9865db36fae9d
Trivium0911 <triv_1993@mail.ru>
2024-09-28 15:43:07 +0300

    update model Bd, add verbose names and change plural verbose name

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 
 class Bd(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(default=' ')
-    price = models.FloatField(default=0.0)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(default=' ', verbose_name='Описание')
+    price = models.FloatField(default=0.0, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True,
+                                     verbose_name='Дата публицации')
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
 
 
 
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -3,7 +3,7 @@ from .models import Bd
 
 
 def index(request):
-    database= Bd.objects.order_by('-published')
+    database = Bd.objects.order_by('-published')
     return render(request, 'bboard/index.html', {'database': database})
 
 
