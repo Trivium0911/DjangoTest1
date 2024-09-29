896f3047795a0d8ff06026eff532a26fc41c0198
Trivium0911 <triv_1993@mail.ru>
2024-09-29 21:51:17 +0300

    add rubric view, add path for rubric view to urls.py, create rubric_bds.html

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, rubric_bds
 
 urlpatterns = [
+    path('<int:rubric_id>/', rubric_bds),
     path('', index),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from .models import Bd
+from .models import Bd, Rubric
 
 
 def index(request):
     database = Bd.objects.all()
-    return render(request, 'bboard/index.html', {'database': database})
+    rubrics = Rubric.objects.all()
+    context = {'database': database, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
 
+
+def rubric_bds(request, rubric_id):
+    database = Bd.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'database': database, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request, 'bboard/rubric_bds.html', context)
 
